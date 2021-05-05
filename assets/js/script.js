c3a48c276f0fdff3dca138c8b19413fbe04f0858
george572 <g.khutiashvili@gmail.com>
2021-05-05 07:12:06 -0700

    all the other pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,20 @@ navigationToggler.addEventListener("click", function () {
     hiddenNav.classList.toggle("nav-active");
     navigationToggler.classList.toggle("toggler-active")
 })
+
+//map
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: -25.344, lng: 131.036 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+    });
+}
